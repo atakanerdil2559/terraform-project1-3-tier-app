0f7d94c00a225262c9251de7371fd31cf7abfaa5
sezgin mutlu <sezginmutlu6@gmail.com>
2020-03-27 22:53:37 +0000

    added

--- data_source.tf
+++ data_source.tf
@@ -21,18 +21,3 @@ data "terraform_remote_state" "DB"  {
 }
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
